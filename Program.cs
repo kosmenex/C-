c8dca8abbd48dcfb0f0ccd313526e354e01f8a43
Kübra KÖSMENE <64540049+kosmenex@users.noreply.github.com>
2023-03-15 08:01:13 +0300

    C# 1. DERS

---
+++ Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //C# EĞİTİMİ DERS NOTLARI VE UYGULAMLARIM 
+            //    KÜBRA KÖSMENE İSTANBUL ÜNİVERSİTESİ FİZİK LİSANS ÖĞRENCİSİ
+            //görmüş olduğunuz çift slaş işareti yorum satırları oluşturur,
+            //bunları yorum ve açıklama yapmak istediğimiz zamanlar kullanırız
+            //derleyici bunları kod satırı olarak görmez
+            //CTRL+K+C yorum satırı açma kısayoludur
+            //CTRL+K+U yorum satırı kapama kısayoludur
+            //CTRL+D bulunduğunuz satırı aynen kopyalama kısayoludur
+
+
+            /*
+            
+            UZUN METİNLİ KAÇIŞ KARAKTERİDİR
+ 
+             */
+
+            // VERİ TİPLERİ
+            int sayi = 5;
+            string isim = "kübra";
+            double maas = 55.9;
+            bool dogrumu=false; //veya 
+            bool yanlısmı = true;
+            char harf = 'a';
+
+            //bool koşul ifadesidir, 1 veya 0 değeri döndüren koşulların sonuçlarını döndürür
+
+            int a = 1;
+            int b = 2;
+            bool kontrol_1 = a > b;
+            bool kontrol_2=b > a;
+            Console.WriteLine(kontrol_1);
+            Console.WriteLine(kontrol_2);
+            Console.ReadLine();
+
+            string ad = "kübra";
+            string soyisim = "kösmene";
+            Console.WriteLine(isim+" "+soyisim);
+            Console.ReadLine();
+
+            //DEĞİŞKEN TANIMLAMA
+            /*
+             * VERİ_TİPİ DEĞİŞKEN_ADİ=DEĞİŞKEN_DEĞERİ;
+             * değişkenadi için belirli kurallar vardır
+             * sayıyla başlayamaz
+             * özel karakterle başlayamaz
+             * tanımlı isimler kullanılamaz
+             * camelCase
+             * Pascalcase
+             */
+            Console.WriteLine("yiğit\nkumbasar");
+            Console.WriteLine("yiğit\tkumbasar");
+            Console.ReadLine();
+            const double p = 3.4; //sabit sayıları hafızada değişmeden tutuyor
+            Console.WriteLine(p);
+       
+
+            int x;
+            x = 55;
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+            //{
+            //    int sayi1 = 13;
+            //    {
+            //        long sayi1 = 13;
+            //    }
+            //    sayi1 = 23;
+            //}
+
+            /* ARİTMERİK OPERATÖRLER*/
+            int sayi_1 = 20;
+            int sayi_2 = 30;
+            Console.WriteLine
+                (
+                "toplam=" + sayi_1 + sayi_2, "\n",
+                "fark=", sayi_1 - sayi_2, "\n",
+                "çarpım=", sayi_1 * sayi_2, "\n",
+                "bölüm=", sayi_1 / sayi_2, "\n",
+                "mod=", sayi_1 % sayi_2, "\n"
+                );
+            sayi_1++;
+            sayi_2--;
+            Console.WriteLine(sayi_1);
+            Console.WriteLine(sayi_2);
+            Console.ReadLine();
+
+
+            /* MANTIKSAL OPERATÖRLER
+             * && VE manasında || VEYA manasında ! DEĞİL manasında kullanılır*/
+
+            bool a_1 = true;
+            bool a_2 = false;
+            Console.WriteLine(a_1&&a_2);
+            Console.WriteLine(a_1||a_2);
+            Console.WriteLine(!a_1);
+            Console.WriteLine(!a_2);
+            Console.ReadLine();
+
+            /* İŞLEMLİ ATAMA OPERATÖRLERİ */
+
+            int sayı1 = 10;
+            int sayı2 = 15;
+            sayı1 = sayı1 - sayı2; //her ikiside aynı işlemi veriyor
+            sayı1 -= sayı2;
+            /* 
+             * a=a-b.........a-=b;
+             * a=a+b.........a+=b;
+             * a=a*b.........a*=b;
+             * a=a/b.........a/=b;
+             * a=a%b.........a%=b;
+             */
+
+            /* KARŞILAŞTIRMA OPERATÖRLERİ
+             * A>B......A B DEN BÜYÜKTÜR
+             * A<B......A B DEN KÜÇÜKTÜR
+             * A>=B.....A B DEN BÜYÜK VEYA EŞİTTİR
+             * A<=B.....A B DEN KÜÇÜK VEYA EŞİTTİR
+             * A==B.....A B YE EŞİT MİDİR?(SORGULAMA)
+             * A!=B.....A B YE EŞİT DEĞİL Mİ(SORGULAMA)
+             */
+
+            int a_sayısı = 20;
+            int b_sayısı = 35;
+            bool kontrol_= a < b;
+            bool kontrol0= a > b;
+            bool kontrol1= a == b;
+            bool kontrol2= a <= b;
+            bool kontrol3= a >= b;
+            bool kontrol4= a != b;
+            Console.WriteLine(kontrol_,kontrol0,kontrol1,kontrol2,kontrol3,kontrol4);
+            Console.ReadLine() ;    
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
