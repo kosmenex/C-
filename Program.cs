3296883fd954d2e6330bbdb034665da4084f4a23
Kübra KÖSMENE <64540049+kosmenex@users.noreply.github.com>
2023-03-19 23:13:54 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,147 +1,155 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IF_Ornekler
+namespace _0005
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-        //if else çeşitli örnekler
-        //Kullanıcıdan iki ürün fiyatı isteyin,fiyat toplamları 250 tl geçerse
-        //ucuz ürüne
-        //%25 indirim uygulayınız.
+        //Ağrı Derecesi 0->Ağrım yok 1->Çok az ağrım var 2-Biraz ağrım var 3->Ağrım var 4->Şiddetli ağrım var 5->Çok şiddetli ağrım var
+        //Else Hatalı giriş yapıldı.Lütfen tekrar deneyiniz. goto ile tekrar değer girmesini isteyin.
+        Hata:
+            Console.WriteLine("AĞRI DERECENİZ:" + "\n" +
+                "0->Ağrım yok " + "\n" +
+                "1->Çok az ağrım var " + "\n" +
+                "2-Biraz ağrım var " + "\n" +
+                "3->Ağrım var " + "\n" +
+                "4->Şiddetli ağrım var " + "\n" +
+                "5->Çok şiddetli ağrım var ");
 
-        Geri:
-            Console.WriteLine("1.ürün fiyatı=");
-            double urun_1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("2.ürün fiyatı=");
-            double urun_2 = Convert.ToDouble(Console.ReadLine());
-            double toplam = urun_1 + urun_2;
-
-            if (toplam > 250)
+            int AgrıDerecessı = Convert.ToInt32(Console.ReadLine());
+            if (AgrıDerecessı == 0)
             {
-                if (urun_1 > urun_2)
+                Console.WriteLine("Ağrım yok");
+            }
+            else if (AgrıDerecessı == 1)
+            {
+                Console.WriteLine("Çok az ağrım var");
+            }
+            else if (AgrıDerecessı == 2)
+            {
+                Console.WriteLine("Biraz ağrım var");
+            }
+            else if (AgrıDerecessı == 3)
+            {
+                Console.WriteLine("Ağrım var");
+            }
+            else if (AgrıDerecessı == 4)
+            {
+                Console.WriteLine("Şiddetli ağrım var");
+
+            }
+            else if (AgrıDerecessı == 5)
+            {
+                Console.WriteLine("Çok şiddetli ağrım var");
+            }
+            else
+            {
+                Console.WriteLine("Hatalı Giriş Tekrar Deneyiniz!");
+                goto Hata;
+
+            }
+        // CHAR DÖNÜŞÜMÜNÜ MUTLAKA HOCAYA SOR
+        //
+        Don:
+            Console.WriteLine("AĞRI DERECENİZ:" + "\n" +
+              "0->Ağrım yok " + "\n" +
+              "1->Çok az ağrım var " + "\n" +
+              "2-Biraz ağrım var " + "\n" +
+              "3->Ağrım var " + "\n" +
+              "4->Şiddetli ağrım var " + "\n" +
+              "5->Çok şiddetli ağrım var ");
+            // int deger=Convert.ToInt32(Console.ReadLine());
+            char deger = Convert.ToChar(Console.ReadLine());
+
+            if (char.IsDigit(deger))
+            {
+                int deger = Convert.ToInt32(deger);
+                if (deger == 0)
                 {
-                    urun_2 = (urun_2 * 75) / 100;
-                    Console.WriteLine($"{urun_2}'ye %25 İndirim uygulandı");
-                    toplam = urun_1 + urun_2;
-                    Console.WriteLine($"toplam={toplam}");
+                    Console.WriteLine("Ağrı yok");
+
+                }
+                else if (deger == 1)
+                {
+                    Console.WriteLine("Ağrım çok az");
+                }
+                else if (deger == 2)
+                {
+                    Console.WriteLine("Biraz ağrım var");
+
+                }
+                else if (deger == 3)
+                {
+                    Console.WriteLine("Ağrım var");
+                }
+                else if (deger == 4)
+                {
+                    Console.WriteLine("Şiddetli");
                 }
                 else
                 {
-                    urun_1 = (urun_1 * 75) / 100;
-                    Console.WriteLine($"{urun_1}'ye %25 İndirim uygulandı");
-                    toplam = urun_1 + urun_2;
-                    Console.WriteLine($"toplam={toplam}");
+                    Console.WriteLine("Çok şiddetli ağrım var");
                 }
 
+
             }
             else
             {
-                Console.WriteLine($"toplam={toplam}");
-
+                Console.WriteLine("Yanlış Karakter Girdiniz!");
+                goto Don;
             }
+
+            // KULLANICI ŞİFRE GİRİŞİ
+
+
+            string kA = "kübra";
+            string posta = "kk@gmail.com";
+            string sifre = "123";
+        Don:
+            Console.WriteLine("kullanıcı adınız");
+            string kullanıcı = Console.ReadLine();
+            Console.WriteLine("mailiniz:");
+            string mail = Console.ReadLine();
+            Console.WriteLine("şifreniz");
+            string parola = Console.ReadLine();
+
+            if (kA == kullanıcı && sifre == parola || posta == mail && sifre == parola)
+            {
+                Console.WriteLine($"Hoşgeldiniz Sayın {kullanıcı}");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Başarısız");
+                goto Don;
+            }
+
+
+
+            //Kullanıcıdan iki tane sayı alıp bunlarn farkını alın
+            //AMA fark negatif olmayacak.
+            Console.WriteLine("Sayi 1");
+
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sayi 2");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+            int sonuc = sayi1 - sayi2;
+            if (sonuc < 0)
+            {
+                sonuc *= -1;
+                Console.WriteLine(sonuc);
+            }
+
+
             Console.ReadKey();
-            goto Geri;
-
-        /*
-            Kullanıcıdan alınacak kw değerine göre.
-            KW başına değer                                   
-            0-100 kw harcamışsa 0.35 ile fiyatlanıdırlacak.
-            101-250 kw => 0.38 -> 
-            251-400 kw=> 0.41 ->
-            401-600 kw=> 0.42 ->
-            600 kw  üstü ise 120 tl indirim yapılacak. ve fiyatlandırma 0.43 üzerinden olacak.İndirim Oranı ekrana yazdırılsın.
-            120 tl indirim yapıldı diye ekrana bilgi mesajı indirim yaptırabilirsiniz.
-            Örnek -> 150 kw elektrik kullanılmış olsun 150 * 0.38 yapılacak.
-            ilk koşulumuz zaten sıfırdan büyük veya eşit koşulu olduğu için negatif değer girmesi söz konusu değildir.ü
-        */
-
-            // her iki örneğimizde de goto kullandığımız için alt satırlar silik renkli çıkıyor
-            // çünkü GOTO döngüsü sonsuza kadar çalışıyor ve alt satırlara derleyici asla geçemiyor
-
-        Back:
-            Console.WriteLine("kaç kw elektrik kullandınız?");
-            double kw=Convert.ToDouble(Console.ReadLine());
-            
-            if (kw>=0 && kw<=100)
-            {
-                double indirim=(kw*35)/100;
-                Console.WriteLine($"Tebrikler {indirim} tl indirim kazandınız");
-                double tutar = (kw * 65) / 100;
-                Console.WriteLine($"toplam tutar={tutar} tl");
-            }
-            else if (kw>=101 && kw<=250)
-            {
-                double indirim = (kw * 38) / 100;
-                Console.WriteLine($"Tebrikler {indirim} tl indirim kazandınız");
-                double tutar = (kw*62) / 100;
-                Console.WriteLine($"toplam tutar={tutar} tl");
-
-            }
-            else if (kw>=251 && kw<=400)
-            {
-                double indirim = (kw * 41) / 100;
-                Console.WriteLine($"Tebrikler {indirim} tl indirim kazandınız");
-                double tutar = (kw * 59) / 100;
-                Console.WriteLine($"toplam tutar={tutar} tl");
-            }
-            else if(kw>=401 && kw<=600) 
-            {
-                double indirim = (kw * 42) / 100;
-                Console.WriteLine($"Tebrikler {indirim} tl indirim kazandınız");
-                double tutar = (kw * 58) / 100;
-                Console.WriteLine($"toplam tutar={tutar} tl");
-            }
-            else if (kw>=601)
-            {
-                Console.WriteLine("Tebrikler 120 tl indirim kazandınız");
-                double tutar = ((kw * 57) / 100) - 120;
-                Console.WriteLine($"toplam tutar={tutar}");
-
-            }
-            else
-            {
-                Console.WriteLine("yanlış karakter girdiniz");
-            }
-            goto Back;
-
-            Console.ReadKey();  
             
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+               
+             
 
 
 
